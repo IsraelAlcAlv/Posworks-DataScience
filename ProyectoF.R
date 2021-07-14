263ccb217f0930c3352079409f46e71586bb3ed5
Israel Alcocer Alvarez <isra961998@gmail.com>
2021-07-14 14:43:57 -0500

    Proyecto Finalizado

--- ProyectoF.R
+++ ProyectoF.R
@@ -1,10 +1,15 @@
 # EQUIPO 1
 
+dir()
+setwd()
 #PostWorks: Programaci√≥n y Estad√≠stica con R - BEDU
 
 rm(list = ls())
 library(ggplot2)
 library(dplyr)
+library(fbRanks)
+library(DescTools)
+
 
 ##################### Introducci√≥n a R y Software #############################
 
@@ -281,3 +286,104 @@ pnorm(1.01, mean = mean(cociente.boot),sd = sd(cociente.boot)) -
 # (que el cociente sea igual a uno) es solo de 3.46% por lo que podemos concluir
 # que en general que un equipo anote "x" cantidad de goles depende de los goles 
 # anotados por el equipo contrario.
+
+
+#################   RegresiÛn Lineal y ClasificaciÛn   ######################
+
+SP3<-select(SP3,Date, HomeTeam, FTHG, AwayTeam, FTAG)
+SP2<-select(SP2,Date, HomeTeam,  FTHG, AwayTeam, FTAG)
+SP1<-select(SP1,Date, HomeTeam,  FTHG, AwayTeam, FTAG)
+
+View(SP1); View(SP2); View(SP3)
+
+# 4: Cambio de formatos de las fechas:
+
+SP1<-mutate(SP1, Date=as.Date(Date,"%d/%m/%Y"))
+SP2<-mutate(SP2, Date=as.Date(Date,"%d/%m/%Y"))
+SP3<- mutate(SP3, Date=as.Date(Date,"%d/%m/%y")) 
+
+# Uni√≥n de los Data Frames
+
+SmallData<-rbind(SP3,SP2,SP1)
+
+
+SmallData <- rename(SmallData, date = Date, home.team = HomeTeam, home.score = FTHG, 
+                    away.team = AwayTeam, away.score = FTAG)
+
+write.csv(SmallData, "soccer.csv", row.names = F)
+
+#install.packages("fbRanks")
+
+listasoccer <- create.fbRanks.dataframes(scores.file = "soccer.csv")
+str(listasoccer)
+
+anotaciones <- listasoccer$scores
+equipos <- listasoccer$teams
+
+str(anotaciones)
+str(equipos)
+
+rank.teams(scores = anotaciones,teams=equipos)
+
+?unique
+fecha <- unique(anotaciones$date)
+str(fecha)
+fecha - sort(fecha)
+
+n <- length(fecha)
+?rank.teams
+ranking <- rank.teams(scores = anotaciones,teams=equipos,max.date = fecha[n-1], 
+           min.date = fecha[1])
+
+
+Predicciones <- predict(ranking, date=fecha[n])
+
+
+########################  Series de Tiempo  ####################################
+#Postwork 6
+
+#Descargar data
+soccer.data <- read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-R-Santander-2021/main/Sesion-06/Postwork/match.data.csv")
+
+#Modificando formato fecha y agregando suma de goles
+soccer.data <- mutate(soccer.data, date = as.Date(date, "%Y-%m-%d"),
+                      sumagoles = home.score + away.score, juegos = 1)
+
+#Agrupando por aÒo y mes y formato de columnas
+monthAvgGoals <- soccer.data %>% group_by(Year(date), Month(date, fmt = "mm"))%>%
+        summarise(mean = mean(sumagoles))
+names(monthAvgGoals) <- c("year","month", "avrgGoals")
+
+#Acotando datos de 2010 a 2019
+ts.dataset <- as.data.frame(monthAvgGoals[- c(97:101),])
+
+#Creando ts y graficando
+avgSumaGoles.ts <- ts(ts.dataset$avrgGoals, start = 2010, frequency = 10)
+
+plot(avgSumaGoles.ts, xaxt = "n", main= "Promedio de suma de goles mensual",
+     ylab="Promedio", xlab="Temporadas")
+axis(1, at = seq(from = 2010, to = 2019, by=1) , 
+     labels = paste("Aug-", seq(from = 2010, to = 2019, by=1), sep = ""))
+abline(v=seq(from = 2010, to = 2019, by=1), col="black", lty=2, lwd=1)
+abline(h = mean(ts.dataset$avrgGoals), col = "red", lwd=2)
+
+
+####################  Conexiones con BD's y Datos Externos  ####################
+
+#Conectandose a MongoDB
+#Recuerda configurar tu cluster segun tu IP
+dmd <- mongo(
+        collection = "match",
+        db = "match_games",
+        url = "mongodb+srv://nachorz:contraseÒa@cluster0.wmpq4.mongodb.net/test",
+)
+#Creando Query de los juegos de local el 20 de Dic del 2015 del real madrid
+localdata <- dmd$find(
+        query = '{"home.team" : "Real Madrid", "date":"2015-12-20"}',
+        limit = 1)
+#Creando Query de los juegos de visita el 20 de Dic del 2015 del real madrid
+visitdata <-  dmd$find(
+        query = '{"away.team" : "Real Madrid", "date":"2015-12-20"}',
+        limit = 1)
+print(visitdata)
+print(localdata)
